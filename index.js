2d8fe679816a622138a32717d58dc51cd0a30c7e
berotburns <justin.berotburns@gmail.com>
2021-01-17 21:58:24 -0800

    Updating favicon and npm modules

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { nextTick } = require('process');
+const favicon = require('serve-favicon');
+const path = require('path');
+
 const { readFile } = require('fs').promises;
 const data = require('./src/readdata.js')
 
 const app = express();
 
-app.use(express.favicon('./favicon.ico'));
+app.use(favicon(path.join(__dirname, '', 'favicon.ico')));
 
 app.get('/', async(request, load, response) => {
 
